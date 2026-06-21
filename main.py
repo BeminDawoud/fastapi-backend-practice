4d1a8d96e7fd8856366f615c24c6e7111ae385f0
Bemin Dawoud <69398996+BeminDawoud@users.noreply.github.com>
2026-06-21 12:20:10 +0300

    addinga a second get method -/posts-

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message":"Welcome to my API!!!"}
+    return {"message":"Welcome to my API!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"posts": ["Post 1", "Post 2", "Post 3"]}
