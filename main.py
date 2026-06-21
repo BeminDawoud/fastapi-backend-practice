4b4fd72c1ecb75527b60f2012553f8907289bf79
Bemin Dawoud <69398996+BeminDawoud@users.noreply.github.com>
2026-06-22 01:49:12 +0300

    Adding HTTP Responses and handling error for getting a post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -15,34 +15,44 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
-my_posts = [{"title": "Post 1", "content": "Content of post 1", "published": True, "rating": 5},
-            {"title": "Post 2", "content": "Content of post 2", "published": False, "rating": 4}]
+my_posts = [{"title": "Post 1", "content": "Content of post 1", "published": True, "rating": 5, "id": 1},
+            {"title": "Post 2", "content": "Content of post 2", "published": False, "rating": 4, "id": 2}]
 
 
-# creating a root endpoint that returns a welcome message
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+# an endpoint that returns a welcome message
 @app.get("/")
 def root():
     return {"message":"Welcome to my API!"}
 
 
-# creating a posts endpoint that returns a list of posts
+# an endpoint that returns a list of posts
 @app.get("/posts")
 def get_posts():
     return {"posts": my_posts}
 
 
 
-# creating a post endpoint that takes a post as a path parameter that returns a message of the post created successfully
-@app.post("/create_post/{post}")
-def create_post(post: str):
-    return {"message": f"Post '{post}' created successfully!"}
 
-
-# creating a post with a schema that takes a post as a body parameter that returns the post created successfully
-@app.post("/createPost")
+# an endpoint that takes a post with a schema as a body parameter and returns the post created successfully
+@app.post("/createPost", status_code= status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     # not using a database yet, so we will generate a random id for the post
     post_dict["id"] = randrange(0, 1000000)
     my_posts.append(post_dict)
     return {"Data": post.dict()}
+
+
+
+# an endpoint that takes a path parameter and returns the post with that id
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
+    return {"post_detail": post}
